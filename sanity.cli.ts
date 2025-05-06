6e553dc871286aa1f2d6da9c34499262ba2576e1
Kiran Murugulla <murugull@adobe.com>
2025-05-06 15:51:12 -0400

    correcting configs

--- sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import { defineCliConfig } from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'c973pa4u',
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
-  }
-}) 
